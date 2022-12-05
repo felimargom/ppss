159efd1e0779c3ac67af42d19ca4a22ce53a6b42
agomezguru <alagunas@coati.com.mx>
2022-12-05 14:48:18 -0600

    Creates Form and Block needed to make payments.

---
+++ src/Plugin/Block/PPSSButtonPayBlock.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * @file
+ * Creates a block which displays the PPSSButtonPay contained in PPSSButtonPay.php
+ */
+
+namespace Drupal\ppss\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Access\AccessResult;
+
+/**
+ * Provides the PPSS main block.
+ *
+ * @Block(
+ *   id = "ppss_button_pay",
+ *   admin_label = @Translation("The PPSS Button Pay")
+ * )
+ */
+class PPSSButtonPayBlock extends BlockBase
+{
+  /**
+   * {@inheritdoc}
+   */
+  public function build()
+  {
+
+    return \Drupal::formBuilder()->getForm('Drupal\ppss\Form\PPSSButtonPay');
+
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockAccess(AccountInterface $account)
+  {
+    // If viewing a node, get the fully loaded node object.
+    $node = \Drupal::routeMatch()->getParameter('node');
+    $allowedNodeTypes = \Drupal::config('ppss.settings')->get('content_types');
+
+    if (!(is_null($node))) {
+      //if (strlen($node->gettype() == 'article') {
+        return AccessResult::allowedIfHasPermission($account, 'view ppss button');
+      //}
+      
+    }
+
+    return AccessResult::forbidden();
+
+  }
+}
