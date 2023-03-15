c6ba7e8a98b5a1b24cbc8b5f8c52e2bb7052ab43
Alejandro Gomez <alagunas@coati.com.mx>
2023-03-15 15:11:02 -0600

    Develop code approved (#13)
    * validate payment completed
    
    * Translate comments write in spanish and clean code with linter.
    
    More rework is needed.
    
    ---------
    
    Co-authored-by: lauraRa <ramoslaura418@gmail.com>

--- src/WebhookCrudManager.php
+++ src/WebhookCrudManager.php
@@ -14,7 +14,8 @@ use Drupal\Core\Config\ConfigFactory;
  * Entity CRUD operations in response to webhook notifications.
  *
  */
-class WebhookCrudManager {
+class WebhookCrudManager
+{
 
   /**
    * The manager service.
@@ -36,17 +37,19 @@ class WebhookCrudManager {
    * @param \Psr\Log\LoggerInterface $logger
    *   A logger instance.
    */
-  public function __construct(EntityTypeManagerInterface $entity_type_manager, ClientInterface $http_client) {
+  public function __construct(EntityTypeManagerInterface $entity_type_manager, ClientInterface $http_client)
+  {
     $this->entityTypeManager = $entity_type_manager;
     $this->httpClient = $http_client;
   }
 
   /**
-   * 
+   *
    * @return array $data
    *   cancel subscription.
    */
-  public function cancelSubscription($id) {
+  public function cancelSubscription($id)
+  {
     //obtener los datos de la venta
     $query = \Drupal::database()->select('ppss_sales', 's');
     $query->join('ppss_sales_details', 'sd', 's.id = sd.sid');
@@ -55,75 +58,77 @@ class WebhookCrudManager {
     $query->fields('sd',['id', 'created']);
     $query->orderBy('created', 'DESC');
     $results = $query->execute()->fetchAll();
-    if(count($results) > 0 ) {
-      $subscription = $results[0];//get the last payment
-      //calcular la fecha de vencimiento
-      //a la ultima fecha de pago sumar +1 frecuencia(mes/año)
-      $expire = strtotime(date('d-m-Y',$subscription->created). ' + 1 '.$subscription->frequency.'');
+    if (!count($results) > 0) {
+      $subscription = $results[0]; // get the last payment
+      // Validate supscription end date
+      // o last payment date add +1 frecuency(month/year)
+      $expire = strtotime(date('d-m-Y', $subscription->created). ' + 1 '.$subscription->frequency.'');
       $today = date('d-m-Y');
-      //Validación de fecha de expiración con fecha actual
-      if(date('d-m-Y',$expire) == $today) {
-        //actualizar la tabla de ppss_sales
+      // Validate expiration date with current expiration date
+      if (date('d-m-Y', $expire) == $today) {
+        // Update ppss_sales table
         \Drupal::database()->update('ppss_sales')->fields([
           'status' => 0,
-          'expire' => $expire,
-        ])->condition('id_subscription', $id, '=')->execute();
+          'expire' => $expire, ])->condition('id_subscription', $id, '=')->execute();
   
-        //Eliminar rol asignado de la suscripción del usuario 
+        // Remove user role added by subscription purchased
         try {
-          $user = \Drupal\user\Entity\User::load($subscription->uid); 
+          $user = \Drupal\user\Entity\User::load($subscription->uid);
           $user->removeRole($subscription->id_role);
           $user->save();
-          $msg_info = 'Se ha cancelado la suscripción de plan '.$subscription->id_role.' del usuario '.$subscription->uid;
+          $msg_info = 'Se ha cancelado la suscripción de plan '.$subscription->id_role.
+            ' del usuario '.$subscription->uid;
           $msg_user = 'Tu suscripción ha sido cancelada, ';
         } catch (\Exception $e) {
           \Drupal::logger('PPSS')->error($e->getMessage());
         }
       } else {
-        //actualizar la tabla de ppss_sales
+        // Update ppss_sales table
         \Drupal::database()->update('ppss_sales')->fields([
-          'expire' => $expire,
-        ])->condition('id_subscription', $id, '=')->execute();
-        $msg_info = 'Se ha programado con fecha '.date('d-m-Y',$expire).' la cancelación de la suscripción de plan '.$subscription->id_role.' del usuario '.$subscription->uid;
-        $msg_user = 'Tu suscripción ha sido programada para cancelar el día '.date('d-m-Y',$expire);
+          'expire' => $expire,])->condition('id_subscription', $id, '=')->execute();
+        $msg_info = 'Se ha programado con fecha '.date('d-m-Y', $expire).
+          ' la cancelación de la suscripción de plan '.$subscription->id_role.
+          ' del usuario '.$subscription->uid;
+        $msg_user = 'Tu suscripción ha sido programada para cancelar el día '.date('d-m-Y', $expire);
       }
-      //validar tipo de rol enterprise de plan Negocio
-      if($subscription->id_role == 'enterprise') {
-        //despublicar anuncios -asignar fecha de despublicación
-        $nids = \Drupal::entityQuery("node")->condition('uid', $subscription->uid)->condition('type', 'nvi_anuncios_e')->execute();
+      // Validate role type 'enterprise' of 'Plan Negocio'
+      if ($subscription->id_role == 'enterprise') {
+        // Unpublish all ads setting a new date in the future
+        $nids = \Drupal::entityQuery("node")->condition('uid', $subscription->uid)
+          ->condition('type', 'nvi_anuncios_e')->execute();
         $entity = \Drupal::entityTypeManager()->getStorage("node");
         $nodes = $entity->loadMultiple($nids);
         foreach ($nodes as $node) {
-          //$node->setUnpublished(true)->save();
           $node->unpublish_on = $expire;
           $node->save();
         }
       }
       \Drupal::logger('PPSS')->info($msg_info);
       $msg = '<div style="text-align: center;  margin: 20px;">
-      <h1> ¡Hasta pronto! </h1>
-      <h1> !Cancelación de suscripción en Encuéntralo! &#128522;</h1>
-      <br><div style="text-align: center; font-size: 24px;">
-    '.$msg_user.' mientras tanto recuerda que puedes continuar publicando anuncios con la versión gratuita.
-      </div><br><br>
-      <div style="text-align: center; border-top: 1px solid #bdc1c6; padding-top: 20px; font-style: italic; font-size: medium; color: #83878c;"><br>--  El equipo de Encuéntralo</div></div>';
+        <h1> ¡Hasta pronto! </h1>
+        <h1> !Cancelación de suscripción en Encuéntralo! &#128522;</h1>
+        <br><div style="text-align: center; font-size: 24px;">'.$msg_user.
+        ' mientras tanto recuerda que puedes continuar publicando anuncios con la versión gratuita.
+        </div><br><br>
+        <div style="text-align: center; border-top: 1px solid #bdc1c6; padding-top: 20px; font-style: italic; font-size: medium; color: #83878c;">
+        <br>--  El equipo de Encuéntralo</div></div>';
 
-      //Enviar correo a usuario y marketing
+      // Send alert by email to stakeholders
       $module = 'ppss';
       $key = 'cancel_subscription';
-      $to = $subscription->mail."; marketing@noticiasnet.mx";
+      $to = $subscription->mail.";".\Drupal::config('system.site')->get('mail');
       $params['message'] = $msg;
       $params['subject'] = "Cancelación de suscripción - Encuéntralo";
       $langcode = \Drupal::currentUser()->getPreferredLangcode();
       $send = true;
       $result = \Drupal::service('plugin.manager.mail')->mail($module, $key, $to, $langcode, $params, NULL, $send);
-      if ($result['result'] != true) {
-      $msg_mail = t('There was a problem sending your message and it was not sent.');
-    }
-    else {
-      $msg_mail = t('Your email has been sent.');
-    }
-    \Drupal::logger('PPSS')->info($msg_mail);
+      if (!$result['result']) {
+        $msg_mail = t('There was a problem sending your message and it was not sent.');
+      }
+      else {
+        $msg_mail = t('Your email has been sent.');
+      }
+      \Drupal::logger('PPSS')->info($msg_mail);
     }
   }
 
@@ -140,29 +145,38 @@ class WebhookCrudManager {
     $results = $query->execute()->fetchAll();
     $subscription = $results[0];
     $details = json_decode($subscription->details);
+    
     try {
-      $query = \Drupal::database()->insert('ppss_sales_details');
-      $query->fields(['sid', 'tax', 'price', 'total', 'created', 'event_id']);
-      $query->values([
-        $subscription->id,
-        $details->plan->payment_definitions[0]->charge_models[0]->amount->value,
-        $details->plan->payment_definitions[0]->amount->value,
-        $data->resource->amount->total,
-        strtotime($data->create_time),
-        $data->id
-      ]);
-      $query->execute();
+      $payment = \Drupal::database()->select('ppss_sales_details', 's')->condition('sid', $subscription->id)->condition('event_id', 0)->fields('s')->execute()->fetchAll();
+      if(count($payment) == 1){
+        \Drupal::database()->update('ppss_sales_details')->fields([
+          'event_id' => $data->id,
+        ])->condition('sid', $payment[0]->id, '=')->execute();
+      } else {
+        $query = \Drupal::database()->insert('ppss_sales_details');
+        $query->fields(['sid', 'tax', 'price', 'total', 'created', 'event_id']);
+        $query->values([
+          $subscription->id,
+          $details->plan->payment_definitions[0]->charge_models[0]->amount->value,
+          $details->plan->payment_definitions[0]->amount->value,
+          $data->resource->amount->total,
+          strtotime($data->create_time),
+          $data->id
+        ]);
+        $query->execute();
+      }
     } catch (\Exception $e) {
       \Drupal::logger('PPSS')->error($e->getMessage());
     }
   }
 
   /**
-   * 
+   *
    * @param $id
    *   cancel subscription from encuentralo use the PayPal REST API server.
    */
-  public function cancelSubscriptionE($id, $reason) {
+  public function cancelSubscriptionE($id, $reason)
+  {
     //validar que exista la suscripción y que este activa
     $query = \Drupal::database()->select('ppss_sales', 's');
     $query->condition('id', $id);
@@ -199,10 +213,11 @@ class WebhookCrudManager {
   }
 
   /**
-   * 
+   *
    *  Get access token for use the PayPal REST API server.
    */
-  private function accessToken() {
+  private function accessToken()
+  {
     $config = \Drupal::config('ppss.settings');
     $clientId = $config->get('client_id');
     $secret = $config->get('client_secret');
@@ -219,16 +234,17 @@ class WebhookCrudManager {
   }
 
   /**
-   * 
+   *
    *  Get url paypal for use the PayPal REST API server.
    */
-  private function url_paypal() {
+  private function url_paypal()
+  {
     $config = \Drupal::config('ppss.settings');
-    if ($config->get('sandbox_mode') == TRUE) {
+    if ($config->get('sandbox_mode') == TRUE)
+    {
       return 'https://api-m.sandbox.paypal.com';
     } else {
       return 'https://api-m.paypal.com';
     }
   }
-
 }
